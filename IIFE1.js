5fcb12cbde55533031150ce2726b10622e8b0d3b
Ed Sadler <ed_lorie@comcast.net>
2017-05-01 15:50:27 -0500

    added a file

---
+++ IIFE1.js
@@ -0,0 +1,36 @@
+"use strict";
+
+var Chatty = (function(){
+  var messageRequest = new XMLHttpRequest();
+  messageRequest.addEventListener("load", messageRequestComplete);
+  messageRequest.addEventListener("error", messageRequestFailed);
+
+  function messageRequestComplete(event){
+    console.log("message Data");
+    var messageData = JSON.parse(event.target.responseText);
+    console.log("The message data", messageData);
+    showMessageData(messageData);
+  }
+
+  function messageRequestFailed(event){
+    console.log("error");
+  }
+
+  messageRequest.open("GET", "messages.json");
+  console.log("Get message data");
+
+  messageRequest.send();
+
+  function showMessageData(messageData){
+    var messageDIV = document.getElementById("output");
+    var data = "";
+    var info = messageData.message_info;
+    for (var i = 0; i < info.length; i++){
+      data += `<div>${info[i].message}</div>`
+    }
+    messageDIV.innerHTML += messageData;
+  }
+
+
+
+})(Chatty || {});
--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 console.log("chatty hello");
+
+// var clearButton = document.getElementById("clearAll");
+//   for (var i = 0; i < clearButton.length; i++) {
+//     clearButton[i].addEventListener("click", function(){
+//   })
+// }
+
+var inputField = document.getElementById("inputField")
+inputField.addEventListener("keyup", function(){
+  if(event.charcode === 13){
+// take the inputfield.value put it into a JSON file as an object
+  // add input to json file then re-read the json file and return as an array
+  // write that array to DOM
+}
+})
+
+let addMessage = document.getElementById("inputField");
+addMessage.addEventListener("click", function(){
+  Chatty.addMessage(inputField.value);
+})
