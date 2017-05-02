9024f69205d565b2dc3408b84a042d401f564e84
Ed Sadler <ed_lorie@comcast.net>
2017-05-02 10:59:51 -0500

    added file and made changes

--- IIFE1.js
+++ IIFE1.js
@@ -1,36 +1,56 @@
 "use strict";
 
 var Chatty = (function(){
-  var messageRequest = new XMLHttpRequest();
-  messageRequest.addEventListener("load", messageRequestComplete);
-  messageRequest.addEventListener("error", messageRequestFailed);
+  var messageArray;
+  return {
+    loadMessage : function(){
+      console.log("load message running");
+      var loader = new XMLHttpRequest();
 
-  function messageRequestComplete(event){
-    console.log("message Data");
-    var messageData = JSON.parse(event.target.responseText);
-    console.log("The message data", messageData);
-    showMessageData(messageData);
-  }
-
-  function messageRequestFailed(event){
-    console.log("error");
-  }
-
-  messageRequest.open("GET", "messages.json");
-  console.log("Get message data");
-
-  messageRequest.send();
-
-  function showMessageData(messageData){
-    var messageDIV = document.getElementById("output");
-    var data = "";
-    var info = messageData.message_info;
-    for (var i = 0; i < info.length; i++){
-      data += `<div>${info[i].message}</div>`
+      loader.addEventListener("load", function(){
+        messageArray = JSON.parse(this.responseText);
+        console.log("originalChatty", messageArray);
+      });
+      loader.open("GET", "messages.json");
+      loader.send();
+    },
+    getMessage : function(){
+      return messageArray;
     }
-    messageDIV.innerHTML += messageData;
   }
+})();
+Chatty.loadMessage()
+// .loadChatty();
+//   var messageRequest = new XMLHttpRequest();
+//   messageRequest.addEventListener("load", messageRequestComplete);
+//   messageRequest.addEventListener("error", messageRequestFailed);
+
+//   function messageRequestComplete(event){
+//     console.log("message Data");
+//     var messageData = JSON.parse(event.target.responseText);
+//     console.log("The message data", messageData);
+//     showMessageData(messageData);
+//   }
+
+//   function messageRequestFailed(event){
+//     console.log("error");
+//   }
+
+//   messageRequest.open("GET", "messages.json");
+//   console.log("Get message data");
+
+//   messageRequest.send();
+
+//   function showMessageData(messageData){
+//     var messageDIV = document.getElementById("output");
+//     var data = "";
+//     var info = messageData.message_info;
+//     for (var i = 0; i < info.length; i++){
+//       data += `<div>${info[i].message}</div>`
+//     }
+//     messageDIV.innerHTML += messageData;
+//   }
 
 
 
-})(Chatty || {});
+// })(Chatty || {});
---
+++ add.js
@@ -0,0 +1,12 @@
+"use Strict";
+console.log("add js works");
+
+var userInput = document.getElementById("inputField");
+// var wow = function(){
+
+
+var messages = Chatty.getMessage;
+var message = [];
+document.getElementById("inputField").innerHTML += `<div>${info[i].message}</div>`
+
+
