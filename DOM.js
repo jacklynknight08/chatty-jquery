9369b51bf81f89098aec2b5c389afe3bbbc8ac97
Kyle Kellums <Kyle@Techs-MacBook-Pro.local>
2017-05-03 10:17:57 -0500

    updated DOM.js and add.js

--- DOM.js
+++ DOM.js
@@ -2,9 +2,9 @@ console.log("DOM loaded");
 
 var inputField = document.getElementById("inputField");
 inputField.addEventListener("keyup", function(event){
-	console.log("keyup works", event);
+	// console.log("keyup works", event);
 	if(event.keyCode == 13){
-		console.log("checking keycode");
+		// console.log("checking keycode");
 		Chatty.addMessage(inputField.value);
 	}
 })
@@ -12,6 +12,7 @@ inputField.addEventListener("keyup", function(event){
 function populate (messages) {
 	console.log("are they here", messages);
 	var output = document.getElementById("output");
+
 	for (var i = 0; i < messages.length; i++) {
 		output.innerHTML += `<p>${messages[i]}<button class="delete-btns">Delete</button></p>`;
 	}
@@ -29,3 +30,21 @@ function deleteButtons () {
 }
 Chatty.loadMessage(populate);
 
+/////////////CLEAR ALL///////////////
+    var clearAll = document.getElementById("clearAll");
+    clearAll.addEventListener("click", function(){
+        output.innerHTML = "";
+        clearAll.setAttribute("disabled", true)
+    });
+        //////////DARK THEME//////////
+    var darkTheme = document.getElementById("dark");
+    darkTheme.addEventListener("click", () => {
+        let checkboxTheme = document.querySelector("body");
+        checkboxTheme.classList.toggle("tog-color")
+    });
+        //////////MAKE LARGE//////////
+    var makeLarge = document.getElementById("large");
+    makeLarge.addEventListener("click", () => {
+        let checkboxTheme = document.getElementById("output");
+        checkboxTheme.classList.toggle("tog-large");
+    });
